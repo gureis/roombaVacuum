b9df5071b81a8a8f14254020be8008f39b11b0ee
gureis <gureis4@gmail.com>
2020-06-24 17:56:47 -0300

    add random factor to roomba

--- main.js
+++ main.js
@@ -2,20 +2,45 @@ let rows;
 let columns;
 const root = document.querySelector('#root');
 const roomba = document.createElement('div');
-roomba.style.position = "absolute";
-roomba.style.top = "5px";
-roomba.style.left = "5px";
-roomba.style.backgroundColor = "#eba87f";
-roomba.style.height = "50px";
-roomba.style.width = "50px";
-roomba.style.borderRadius = "40px";
 root.appendChild(roomba);
+const directionEnum = {
+    down: `DOWN`,
+    left: `LEFT`,
+    right: `RIGHT`,
+    up: `UP`
+}
 
-function setupTiles() {
+function setRoombaStartStyle(roomba) {
+    roomba.style.position = "absolute";
+    roomba.style.top = "5px";
+    roomba.style.left = "5px";
+    roomba.style.backgroundColor = "#eba87f";
+    roomba.style.height = "50px";
+    roomba.style.width = "50px";
+    roomba.style.borderRadius = "40px";
+}
+
+function setRoombaJobCompletedStyle(roomba, roombaPos) {
+    roomba.style.top = "5px";
+    roomba.style.left = "5px";
+    roomba.style.backgroundColor = "green";
+    roombaPos.column = 0;
+    roombaPos.row = 0;
+}
+
+function removePreviousTiles() {
+    const tiles = root.getElementsByClassName('tile');
+    
+    while(tiles[0]) {    
+        tiles[0].remove();
+    }    
+}
+
+function setupTiles(totalRows, totalColumns) {
     let top = 60;
     let left = 0;
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
+    for (let i = 0; i < totalRows; i++) {
+        for (let j = 0; j < totalColumns; j++) {
             createTile({
                 top,
                 left
@@ -43,45 +68,109 @@ function createTile(pos) {
 }
 
 function startRoomba() {
+    setRoombaStartStyle(roomba);
     rows = parseInt(document.querySelector('#rows').value) || 2;
-    columns = parseInt(document.querySelector('#columns').value) || 2
-    setupTiles();
+    columns = parseInt(document.querySelector('#columns').value) || 2;
+    removePreviousTiles();
+    setupTiles(rows, columns);
     const roombaPos = {
         column: 0,
         row: 0
     };
+    let lastMovement= "";
     const tiles = document.querySelectorAll('.tile');
-    let currentPos = roombaPos.row;
     const roombaOnId = setInterval(() => {
-        console.log(currentPos);
-        if (rows <= roombaPos.row) {
-            roomba.style.top = "5px";
-            roomba.style.left = "5px";
-            roomba.style.backgroundColor = "green";
+        cleanIfTileIsDirt(tiles, roombaPos, columns);      
+        if (isJobCompleted(tiles)) {
+            setRoombaJobCompletedStyle(roomba, roombaPos);
             clearInterval(roombaOnId);
-        } else if (tiles[currentPos].style.backgroundColor == "rgb(161, 153, 148)") {
-            tiles[currentPos].style.backgroundColor = "#fff";
-        } else if (roombaPos.row % 2 == 0) {
-            if (roombaPos.column < columns - 1) {
-                roombaPos.column++;
-                currentPos++;
-                roomba.style.left = 81 * roombaPos.column + "px";
-            }
-            else {
-                roombaPos.row++;
-                currentPos += columns;
-                roomba.style.top = 100 * roombaPos.row + "px";
-            }
-        } else if (roombaPos.row % 2 != 0) {
-            if (roombaPos.column > 0) {
-                roombaPos.column--;
-                currentPos--;
-                roomba.style.left = 81 * roombaPos.column + "px";
-            } else {
-                currentPos += columns;
-                roombaPos.row++;
-                roomba.style.top = 100 * roombaPos.row + "px";
-            }
-        }
+        } else {
+            const possibleMovements = setPossibleMovements(roombaPos, columns, rows, lastMovement);
+            const movementToDo = randomizeMovement(possibleMovements);
+            lastMovement = movementToDo;
+            executeMovement(movementToDo, roombaPos);
+        } 
     }, 500);
+}
+
+function isJobCompleted(tiles) {
+    for (let index = 0; index < tiles.length; index++) {        
+        if (tiles[index].style.backgroundColor == "rgb(161, 153, 148)") {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isNotTopRow(roombaPos) {
+    if(roombaPos.row == 0) {
+        return false;
+    }
+    return true;
+}
+
+function isNotBottomRow(roombaPos, totalRows) {
+    if(roombaPos.row == totalRows - 1) {
+        return false;
+    }
+    return true;
+}
+
+function isNotLeftColumn(roombaPos) {
+    if (roombaPos.column == 0) {
+        return false;
+    }
+    return true;
+}
+
+function isNotRightColumn(roombaPos, totalColumns) {
+    if (roombaPos.column == totalColumns - 1) {
+        return false;
+    }
+    return true;
+}
+
+function setPossibleMovements(roombaPos, totalColumns, totalRows, lastMovement) {
+    let possibleMovements = [];
+    if(isNotBottomRow(roombaPos, totalRows) && lastMovement != directionEnum.up) {
+        possibleMovements.push(directionEnum.down);
+    }
+    if (isNotTopRow(roombaPos) && lastMovement != directionEnum.down) {
+        possibleMovements.push(directionEnum.up);
+    }
+    if (isNotRightColumn(roombaPos, totalColumns) && lastMovement != directionEnum.left) {
+        possibleMovements.push(directionEnum.right);
+    }
+    if (isNotLeftColumn(roombaPos) && lastMovement != directionEnum.right) {
+        possibleMovements.push(directionEnum.left);
+    }
+    return possibleMovements;
+}
+
+function randomizeMovement(possibleMovements) {
+    const indexOfMmovementToPick = Math.floor(Math.random() * possibleMovements.length);
+    return possibleMovements[indexOfMmovementToPick];
+}
+
+function executeMovement(direction, roombaPos) {
+    if(direction == directionEnum.left) {
+        roombaPos.column--;
+        roomba.style.left = 81 * roombaPos.column + "px";
+    } else if (direction == directionEnum.right) {
+        roombaPos.column++;
+        roomba.style.left = 81 * roombaPos.column + "px";
+    } else if (direction == directionEnum.down) {
+        roombaPos.row++;
+        roomba.style.top = 100 * roombaPos.row + "px";
+    } else if (direction == directionEnum.up) {
+        roombaPos.row--;
+        roomba.style.top = 100 * roombaPos.row + "px";
+    }
+}
+
+function cleanIfTileIsDirt(tiles, roombaPos, totalColumns) {
+    const roombaTile = totalColumns * roombaPos.row + roombaPos.column;
+    if (tiles[roombaTile].style.backgroundColor == "rgb(161, 153, 148)") {
+            tiles[roombaTile].style.backgroundColor = "#fff";
+    }
 }
